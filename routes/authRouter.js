67973e7e5f63e6f5df9bd74d2208cb1410ad6ef5
kiamco <kiamcoo@gmail.com>
2020-01-14 13:30:43 -0800

    web token added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -13,28 +13,6 @@ Router.post('/register', (req, res) => {
     user.password = hash;
 
     /* write function to add user to database */
-     userModel.addUser(user)
-         .then(users => {
-             res.status(201).json({
-                 users,
-                 message: `Successfully created user: ${user.username} the ${user.role}`
-             })
-         })
-         .catch(err => {
-             res.status(400).json({
-                 err,
-                 message: err.errno === 19 ? 'User does not exists' : "Server error"
-             });
-         })
-
-
-});
-
-Router.post('/login', (req, res) => {
-    let { username, password } = req.body;
-
-    /* write function to add user to login*/
-
     userModel.addUser(user)
         .then(users => {
             res.status(201).json({
@@ -45,9 +23,61 @@ Router.post('/login', (req, res) => {
         .catch(err => {
             res.status(400).json({
                 err,
-                message: err.errno === 19 ? 'User exists' : "Server error"
+                message: err.errno === 19 ? 'User does not exists' : "Server error"
             });
         })
+
+
+});
+
+Router.post('/login', (req, res) => {
+    let {
+        username,
+        password
+    } = req.body;
+
+    /* write function to add user to login*/
+
+    let {
+        username,
+        password
+    } = req.body;
+
+    userModel.findByUser(username).then(item => {
+            if (item && bycrypt.compareSync(password, item[0].password)) {
+                const token = genToken(item[0]);
+                res.status(200).json({
+                    username: item[0].username,
+                    token: token
+                });
+            } else {
+                res.status(401).json({
+                    message: "Invalid credentials"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                message: "Server issue"
+            });
+        });
 })
+
+
+function genToken(user) {
+    // create the payload...
+    const payload = {
+        uid: user.uid,
+        username: user.username,
+    };
+
+    const options = {
+        expiresIn: "3h"
+    };
+
+    const token = jwt.sign(payload, secrets.jwtSecret, options);
+    return token;
+}
 
 module.exports = Router;
